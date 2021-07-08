5ad38578a0779c2985f65a27de808ef4d7a911ee
ashish.sharma7 <ashish.sharma7@globallogic.com>
2021-07-08 20:25:26 +0530

    Removed approach 2 to avoid confusion.

--- app/src/main/java/com/codechallenge/maxcharoccurrence/usecase/CalculateMaxCharOccurrenceUseCase.kt
+++ app/src/main/java/com/codechallenge/maxcharoccurrence/usecase/CalculateMaxCharOccurrenceUseCase.kt
@@ -13,56 +13,29 @@ abstract class BaseUseCase<PARAM, RESULT> {
 class CalculateMaxCharOccurrenceUseCase @Inject constructor() : BaseUseCase<String, String>() {
     override suspend fun execute(param: String): String {
         if (param.isEmpty()) return "Enter some text"
-//        Approach 1 using hashmap
         return calculateMaxOccurrences(param)
-//        Approach 2 using array
-//        return getMaxOccurringChar(param.toCharArray())
     }
 
-    //Approach 1
     @VisibleForTesting
     fun calculateMaxOccurrences(text: String): String {
-        val occurrences = mutableMapOf<Char, Int>()
+        val occurrencesMap = mutableMapOf<Char, Int>()
         val result = StringBuilder()
         val value = text.replace(" ", "")
         value.map { char ->
-            val count = occurrences[char]
+            val count = occurrencesMap[char]
             count?.let {
-                occurrences.put(char, count + 1)
+                occurrencesMap.put(char, count + 1)
             } ?: run {
-                occurrences.put(char, 1)
+                occurrencesMap.put(char, 1)
             }
         }
-        val maxCount = Collections.max(occurrences.values)
+        val maxCount = Collections.max(occurrencesMap.values)
 
-        occurrences.map {
+        occurrencesMap.map {
             if (maxCount == it.value) {
                 result.append(" ${it.key} occurred maximum ${it.value}\n")
             }
         }
         return result.toString()
-    }
-
-    //Approach 2
-    private fun getMaxOccurringChar(str: CharArray): String {
-        val freq = IntArray(26)
-        var max = -1
-
-        for (i in str.indices) {
-            freq[str[i] - 'a']++
-        }
-
-        for (i in 0..25) {
-            if (max < freq[i]) {
-                max = freq[i]
-            }
-        }
-        val strResult = StringBuilder()
-        for (i in 0..25) {
-            if (max == freq[i]){
-                strResult.append(" ${(i + 'a'.code).toChar()} occurred maximum $max\n")
-            }
-        }
-        return strResult.toString()
     }
 }
